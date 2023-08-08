109ae6674d02a4207d03d6386e8c13987d56b6d7
Katie Sayers <kate@student.dev-island.org>
2023-08-07 14:05:40 -1000

    got the randomizer functioning

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const box = document.querySelector('.question-box');
-const container = document.querySelector('.container')
-const response = document.querySelector('.response-page')
+const container = document.querySelector('.container');
+const response = document.querySelector('.response-page');
+const gifItem = document.querySelectorAll('.gif-item');
+let gifList = [];
 
-const generate = (e) => {
-    if(e.key === 'Enter') {
-        console.log('it worked')
-    container.classList.add('hidden');
-    response.classList.remove('hidden');
+const randomizer = () => {
+    gifItem.forEach(gif => {
+        gifList.push(gif);
+    })
+    let gifRandom = Math.floor(Math.random() * gifList.length);
+    let gifChoice = gifList[gifRandom];
+    console.log(gifChoice)
+    gifChoice.classList.remove('not-active');
+}
+
+function generate(e) {
+    if (e.key === 'Enter') {
+        container.classList.add('hidden');
+        response.classList.remove('hidden');
+        randomizer();
     }
 }
 
